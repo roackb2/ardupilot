0491cff4388d3633a6694d88e2d58c8bd0d90788
chobits <chobits@itri.org.tw>
2017-06-13 17:07:26 +0800

    send LANDING_TARGET to GCS to indicate irlock target

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -689,6 +689,7 @@ private:
     void send_vfr_hud(mavlink_channel_t chan);
     void send_current_waypoint(mavlink_channel_t chan);
     void send_rangefinder(mavlink_channel_t chan);
+    void send_landing_target(mavlink_channel_t chan);
     void send_proximity(mavlink_channel_t chan, uint16_t count_max);
     void send_rpm(mavlink_channel_t chan);
     void rpm_update();
