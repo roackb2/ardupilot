b5d6994425dace6eb95775b6291347c3cff7e143
chobits <chobits@itri.org.tw>
2017-11-21 17:54:43 +0800

    porting APDS9930 driver

--- libraries/AC_LandProximity/AC_LandProximity.h
+++ libraries/AC_LandProximity/AC_LandProximity.h
@@ -1,7 +1,24 @@
 #include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/Device.h>
+#include <AP_HAL/I2CDevice.h>
+#include <AP_Common/AP_Common.h>
 
 class AC_LandProximity
 {
 public:
     AC_LandProximity();
+    void init();
+    bool proximity;
+private:
+    AP_HAL::OwnPtr<AP_HAL::I2CDevice> _dev;
+    uint8_t getMode();
+    bool setMode(uint8_t mode, uint8_t enable);
+    bool enableLightSensor();
+    bool enableProximitySensor();
+    bool setAmbientLightGain(uint8_t drive);
+    bool setProximityGain(uint8_t drive);
+    bool setLEDDrive(uint8_t drive);
+    bool setProximityDiode(uint8_t drive);
+    bool checkId();
+    void timer();
 };
