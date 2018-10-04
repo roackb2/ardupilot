43ca284f556b9eded3c8d888775380ce309a6ba8
chobits <chobits@itri.org.tw>
2018-10-04 10:43:36 +0800

    GCS_MAVLink: support VISION_SPEED_ESTIMATE

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -637,6 +637,7 @@ private:
     virtual void handle_change_alt_request(AP_Mission::Mission_Command &cmd) = 0;
     void handle_common_mission_message(mavlink_message_t *msg);
 
+    void handle_vision_speed_estimate(mavlink_message_t *msg);
     void handle_vicon_position_estimate(mavlink_message_t *msg);
     void handle_vision_position_estimate(mavlink_message_t *msg);
     void handle_global_vision_position_estimate(mavlink_message_t *msg);
